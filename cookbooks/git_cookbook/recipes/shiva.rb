9f2d7fbe30484bdbfe4d905e4fb9e2e74c7a87c1
Shankar <shiva.shankar@thomsonreuters.com>
2018-07-02 16:47:27 +0530

    new recipe shiva.rb added

---
+++ cookbooks/git_cookbook/recipes/shiva.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook:: git_cookbook
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+package 'Install Apache' do
+  case node[:platform]
+  when 'redhat', 'centos'
+    package_name 'httpd'
+  when 'ubuntu', 'debian'
+    package_name 'apache2'
+  end
+end
