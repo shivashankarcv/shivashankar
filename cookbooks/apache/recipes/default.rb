39774bf16e14ec04c0fa4302f7432465038e6492
shiva <shiva.shankar@thomsonreuters.com>
2018-07-21 18:44:54 +0530

    cookbook roles added

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -1,5 +1,21 @@
 #
-# Cookbook:: apache
+# Cookbook:: git_cookbook
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+package 'Install Apache' do
+  case node[:platform]
+  when 'redhat', 'centos'
+    package_name 'httpd'
+  when 'ubuntu', 'debian'
+    package_name 'apache2'
+  end
+end
+  service "httpd" do
+ action [ :enable, :start ]
+end
+cookbook_file "/var/www/html/index.html" do
+ source "index.html"
+ mode "0644"
+end
---
+++ cookbooks/motd/metadata.rb
@@ -0,0 +1,20 @@
+name 'motd'
+maintainer 'Shiva Shankar'
+maintainer_email 'shivashankar@example.com'
+license "All rights reserved"
+description "Installs/Configures motd"
+#long_description IO.read(File.join(File.dirname(__FILE__), ‘README.md‘))
+version "0.1.0"
+depends "pci"
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/motd/issues'
+
+# The `source_url` points to the development repository for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/motd'
